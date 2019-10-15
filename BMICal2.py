34331f2573a013d4741435bce3cfe9bf4ba192fd
Annapoorani Bala <Arunkumar>
2019-10-15 11:46:07 +0530

    Changed the bmi program

--- BMICal2.py
+++ BMICal2.py
@@ -12,13 +12,13 @@ def bmi():
     "This method implements bmi"
     weight=int(input("Enter the weight in Kg"))
     height=int(input("Enter the height in meter"))
-    bmi=weight/(height*height)
-    print("BMI",bmi)
-    if bmi<=18.5:
+    userbmi=weight/(height*height)
+    print("BMI of the user is ",userbmi)
+    if userbmi<=18.5:
         print("under weight")
-    elif bmi>18.5 and bmi<=24.9:
+    elif userbmi>18.5 and userbmi<=24.9:
         print("normal weight")
-    elif bmi>=25 and bmi<=29.9:
+    elif userbmi>=25 and userbmi<=29.9:
         print("over weight")
     else:
         print("Obese")
