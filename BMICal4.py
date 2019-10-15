d9742d170dad381622a3ef16fa5d0aa3e24b4137
Annapoorani Bala <Arunkumar>
2019-10-15 10:54:48 +0530

    Changed the BMI-4

--- BMICal4.py
+++ BMICal4.py
@@ -1,8 +1,7 @@
 '''BMI Calculator using Exceptional handling to validate Input entry'''
 
-weight=height=1
 def read_value():
-    global weight,height,bmicheck
+    "Returns the users weight and height"
     while True:
         try:
             weight=float(input("Enter the weight in Kg"))
@@ -10,13 +9,17 @@ def read_value():
             break
         except ValueError:
             print("Invalid entry reenter the values....")
+    return weight,height
         
-def cal_bmi():    
+def cal_bmi(weight,height):
+    "Calculates the BMI"    
     bmi=round(weight/(height*height),2)
     print("BMI",bmi)
     return bmi
-def check_bmi():
-    bmicheck=cal_bmi()
+
+def check_bmi(weight,height):
+    "Check the category"
+    bmicheck=cal_bmi(weight,height)
     if bmicheck<=18.5:
         print("under weight")
     elif bmicheck>18.5 and bmicheck<=24.9:
@@ -28,5 +31,5 @@ def check_bmi():
 
 #----START OF SCRIPT
 if __name__=='__main__':
-    read_value()
-    check_bmi()
+    users_weight,users_height = read_value()
+    check_bmi(users_weight,users_height)
